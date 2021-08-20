728610c3ba6945ae8d1186f70388b1d729f7ec28
Kenta57 <0121kenta@gmail.com>
2021-08-20 15:38:35 +0900

    wikipediaのランダムなサイトのタイトルを2つ返すように実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,25 @@
+require "net/http"
+require 'json'
+
 class QuestionsController < ApplicationController
+    # 問題の生成
+    def make_question
+        render json: {items: {title1: get_title, title2: get_title}}
+    end
+
+    # Wikipediaのランダムなタイトルを取得
+    def get_title
+        # MediaWiki APIを利用して, ランダムなwikiのサイトの情報を取得
+        uri = URI.parse("https://ja.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&prop=info")
+        response = Net::HTTP.get_response(uri)
+
+        # サイト情報からタイトルを抽出
+        body = response.body
+        body_hash = JSON.parse(body)
+        title = ""
+        body_hash['query']['pages'].each do |k,v|
+            title = v['title']
+        end
+        return title
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "questions/make_question" => "questions#make_question"
 end
