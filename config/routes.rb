eb7d90a8f45cf1e58e31e898748247fdc3ba1964
Kenta57 <0121kenta@gmail.com>
2021-09-26 22:00:46 +0900

    fix : conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "questions/make_question" => "questions#make_question"
-  get "scraping" => "questions#scraping"
-  get "show/contents" => "questions#get_db"
-  get "show/column/:name" => "questions#get_column_name"
+  get "questions/make_question" => "posts#make_question"
+  get "questions/scraping" => "posts#scraping"
+  get "questions/show/contents" => "posts#get_db"
+  get "questions/show/column/:name" => "posts#get_column_name"
 end
