794c4d82104a6c99e46bc201e0b0640da57da05b
Kenta57 <0121kenta@gmail.com>
2021-10-29 00:57:43 +0900

    dockerから外部のlocalhostにアクセスできるように変更

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,16 +5,16 @@ require "uri"
 class SuggestionsController < ApplicationController
     def show
         #from = "卑弥呼" 
-        #to = "レッドブル" 
+        #to = "レッドブル"
         from = params[:from]
         to = params[:to]
-        uri_before_encode = 'http://192.168.2.117:8000/api/suggestion/' + from + '/' + to
+        uri_before_encode = 'http://gohost:8000/api/suggestion/' + from + '/' + to
         uri = URI(URI.encode uri_before_encode)
         # uri = URI("http://192.168.2.117:8000/api/suggestion/Java/NASA")
         http = Net::HTTP.new(uri.host, uri.port)
         req = Net::HTTP::Get.new(uri)
         res = http.request(req)
         #res.bodyに模範解答のjsonが入ってる
-        render res.body
+        render json: res.body
     end
 end
